3cb650e80d514885768f9e2e6a477d5c1032db49
cheney5311 <czh419779202@163.com>
2025-05-20 02:03:31 +0800

    add btree

---
+++ db/btree_db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"go.etcd.io/bbolt"
+	"os"
+)
+
+type BTreeDB struct {
+	fileName  string
+	mmapBytes []byte
+	fd        *os.File
+	bboltDb   *BBoltDB
+	buckets   map[string]*bbolt.Tx
+}
+
+func NewBtreeDb(dbname string) *BTreeDB {
+	return &BTreeDB{
+		fileName: dbname,
+		bboltDb:  new(BBoltDB).NewInstance(dbname, ""),
+		buckets:  make(map[string]*bbolt.Tx),
+	}
+}
+
+func (db *BTreeDB) AddBtree(name string) error {
+	_, err := db.bboltDb.CreateTable(name)
+	return err
+}
+
+func (db *BTreeDB) Sync() error {
+	return nil
+}
+
+func (db *BTreeDB) Set(btName, key string, value uint64) error {
+	return nil
+}
